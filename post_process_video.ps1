63b69c4de1258cf7d771256ac95df3e2aa19e11e
Qi Wang <qi_wang_zz@outlook.com>
2024-04-17 20:15:40 +0800

    add post video compress

---
+++ post_process_video.ps1
@@ -0,0 +1,23 @@
+# ./compress_cpu_high.ps1 ./inputs/ ./outputs/
+param (
+    [string]$InPath = "./in/",
+    [string]$OutPath = "./out/"
+)
+
+Write-Output "Input Path: $InPath"
+Write-Output "Output Path: $OutPath"
+
+$SuffixList = @(".mp4", ".mkv")
+$OutSuffix = ".mkv"
+
+$SourceFiles = Get-ChildItem $InPath
+
+foreach ($SourceFile in $SourceFiles) {
+  $InPutSuffix = "." + $SourceFile.Name.Split(".")[-1]
+  if ($SuffixList.Contains($InPutSuffix)){
+    Write-Output "Now Processing: $SourceFile"
+    $OutFilePath = $OutPath + $SourceFile.Name.Replace($InPutSuffix, $OutSuffix)
+    Write-Output "Output As: $OutFilePath"
+    ffmpeg -i $SourceFile -map 0:0 -map 0:1 -c:v libx264 -crf 18 -preset:v veryslow -profile:v high -c:a aac -ar 48000 -metadata:s:v:0 'language=zho' -metadata:s:a:0 'language=zho' $OutFilePath
+  }
+}
