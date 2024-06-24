80da2e45f4767de5a5da1a035a648615d021f6b5
Qi Wang <qi_wang_zz@outlook.com>
2024-06-24 11:36:01 +0800

    use veryslow profile

--- process_old_game_capture.ps1
+++ process_old_game_capture.ps1
@@ -11,11 +11,11 @@ $OutSuffix = ".mp4"
 
 # check device
 if ($DeviceName -eq "CPU") {
-  $Device = "libx264"
+  # $Device = "libx264"
 }
 elseif ($DeviceName -eq "GPU_NVIDIA") {
   Write-Output "Warning!!! The compression quality of GPU is generally worse than that of CPU!"
-  $Device = "h264_nvenc"
+  # $Device = "h264_nvenc"
 }
 else {
   Write-Output "Device Name Error!"
@@ -45,9 +45,10 @@ foreach ($SourceFile in $SourceFiles) {
     $OutFilePath = $OutPath + $SourceFile.Name.Replace($InPutSuffix, $OutSuffix)
     Write-Output $OutFilePath
     if ($DeviceName -eq "CPU"){
-      ffmpeg -hide_banner -i $SourceFile -c:v libx264 -crf 20 -preset:v medium -profile:v high -c:a aac  $OutFilePath
+      ffmpeg -hide_banner -i $SourceFile -c:v libx264 -crf 20 -preset:v veryslow -profile:v high -c:a aac  $OutFilePath
     }
     elseif ($DeviceName -eq "GPU_NVIDIA") {
+      # not tested yet
       ffmpeg -hide_banner -i $SourceFile -c:v h264_nvenc -gpu $GPUNumber -cq 20 -multipass 2 -preset:v p7 -profile:v 2 -c:a aac  $OutFilePath
     }
   }
